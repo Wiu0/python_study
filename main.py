813fa56234b5af41d14e8ad6b916e44d7eda5bb2
wiu <williamnoronha@hotmail.com>
2023-10-22 17:41:56 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path='/static')
+
+
+@app.route('/')
+def homepage():
+    return render_template('telalogin.html')
+
+
+@app.route('/cadastrocartao')
+def cadastrocartao():
+    return render_template('cadastrocartao.html')
+
+
+@app.route('/cadastracompras')
+def cadastracompras():
+    return render_template('cadastracompras.html')
+
+
+@app.route('/listacartao')
+def listacartao():
+    return render_template('listacartao.html')
+
+
+@app.route('/relatoriodegastos')
+def relatoriodegastos():
+    return render_template('relatorioDeGastos.html')
+
+
+@app.route('/visualizarfatura')
+def visualizarfatura():
+    return render_template('visualizarFatura.html')
+
+@app.route('/dashboardhome')
+def dashboardHome():
+    return render_template('dashboardhome.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/menu.js
@@ -0,0 +1,12 @@
+/*$(document).ready(function() {
+  $.get("/menu/menu.html", function(data) {
+    $("#menu").html(data);
+  });
+});
+*/
+function checaCookie()
+{
+      alert("Cookies permitidos")
+}
+
+document.onload = checaCookie();
